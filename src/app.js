8407e05a00b932e3cba64c1b29e0682647b8ad45
LuizLiniker <linikerbraz32@gmail.com>
2026-05-18 20:12:59 -0300

    feat: rota POST /login com JWT

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
-const express = require("express")
-const bcrypt = require("bcryptjs") 
+const express = require("express") // biblioteca para rotas
+const bcrypt = require("bcryptjs") //biblioteca para criptografar senha
+const jwt = require("jsonwebtoken") //biblioteca de token para autenticação tela de login
 const supabase = require("./db")
 
 const app = express()
+
+
 
 app.use(express.json())
 
@@ -38,6 +41,40 @@ app.post("/usuarios", async(req,res) => {
     return res.status(201).json(usuarioCriado)//retorna usuario criado
 
 })
+
+app.post("/login", async(req,res) =>{
+    //pega email e senha no body
+    const {email, senha} = req.body
+
+    //busca usuario no banco de dados
+    const {data, error} = await supabase
+    .from("usuarios")
+    .select("*")
+    .eq("email", email)
+    //se não encontrar retorna erro
+    if(!data || data.length === 0) return res.status(401).json({erro: "Email ou senha inválidos"})
+
+    //comparar senha com hash
+    const senhaCorreta = await bcrypt.compare(senha, data[0].senha)
+    
+    
+    if(!senhaCorreta) return res.status(401).json({ erro: "Email ou senha inválidos" })
+    
+    //se não encontrar retorna erro
+    if(error) return res.status(500).json({ erro: error.message })
+
+    const payload = {
+    userId: data[0].id,
+    tipoDeUsuario: data[0].tipo
+}
+
+    const secret = process.env.JWT_SECRET
+    const token = jwt.sign(payload, secret, {expiresIn: "7d"});
+
+    res.json({ token })
+    
+})
+
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000")
